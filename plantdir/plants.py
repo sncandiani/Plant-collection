0ef735f16ee3d7c18c6b2107f6b285746914e22a
sncandiani <sofiancandiani@gmail.com>
2020-07-21 00:11:14 -0500

    using Python request library to fetch from Trefle API

--- plantdir/plants.py
+++ plantdir/plants.py
@@ -1,5 +1,9 @@
-from shamrock import Shamrock
-from apitoken import trefle_token
-api = Shamrock(trefle_token)
-plants = api.plants()
-print(plants)
+import requests
+from ApiToken import trefle_token
+
+# Using Python Requests library to get from the API
+r = requests.get(f'https://trefle.io/api/v1/plants?filter_not%5Bmaximum_height_cm%5D=null&filter%5Bligneous_type%5D=tree&order%5Bmaximum_height_cm%5D=desc&token={trefle_token}')
+jsonResponse = r.json()
+# Iterating through JSON response
+# for key, value in jsonResponse.items():
+#     print(key, ":", value)
